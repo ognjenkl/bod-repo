808e5596077ed6c6dc8f67ec3c4afa7bcb28b7ea
ognjenklincov <ognjenkl@gmail.com>
2023-04-30 14:55:25 +0200

    Test Assignment

--- bod/src/main/java/com/workforce/bod/assignment/Assignment.java
+++ bod/src/main/java/com/workforce/bod/assignment/Assignment.java
@@ -2,11 +2,9 @@ package com.workforce.bod.assignment;
 
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@RequiredArgsConstructor
 @Getter
 public class Assignment {
 
@@ -14,6 +12,18 @@ public class Assignment {
     private final Resource resource;
     private final LocalDateTime start;
     private final LocalDateTime end;
+
+    public Assignment(Task task, Resource resource,
+                      LocalDateTime start, LocalDateTime end) {
+        this.task = task;
+        this.resource = resource;
+        this.start = start;
+        this.end = end;
+
+        if (end.isBefore(start)) {
+            throw new TimeOrderException();
+        }
+    }
 
     public boolean isInTimeInterval(LocalDateTime time) {
         return (start.isBefore(time) || start.isEqual(time))
@@ -23,5 +33,8 @@ public class Assignment {
     public boolean isInTimeCollision(Assignment assignment) {
         return isInTimeInterval(assignment.getStart())
                 || isInTimeInterval(assignment.getEnd());
+    }
+
+    public static class TimeOrderException extends RuntimeException {
     }
 }
---
+++ bod/src/test/java/com/workforce/bod/AssignmentTest.java
@@ -0,0 +1,96 @@
+package com.workforce.bod;
+
+import com.workforce.bod.assignment.Assignment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssignmentTest {
+
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static final String START = "2020-01-01 10:00:00";
+    public static final String END = "2020-01-01 11:00:00";
+
+    Assignment assignment;
+    LocalDateTime startTime;
+    LocalDateTime endTime;
+
+    @BeforeEach
+    void setUp() {
+        startTime = LocalDateTime.parse(START, FORMATTER);
+        endTime = LocalDateTime.parse(END, FORMATTER);
+    }
+
+    @Test
+    void givenAssignment_whenStartIsAfterEndTime_throwException() {
+        assertThrows(Assignment.TimeOrderException.class, () -> new Assignment(null, null, endTime, startTime));
+    }
+
+    @Test
+    void givenTime_whenIsInInterval_theTrue() {
+        assignment = new Assignment(null, null, startTime, endTime);
+
+        String timeToCheck = "2020-01-01 10:30:00";
+        boolean isInInterval = assignment.isInTimeInterval(LocalDateTime.parse(timeToCheck, FORMATTER));
+
+        assertTrue(isInInterval);
+
+    }
+
+    @Test
+    void givenTime_whenIsInInterval_theFalse() {
+        assignment = new Assignment(null, null, startTime, endTime);
+
+        String timeToCheck = "2020-01-01 09:30:00";
+        boolean isInInterval = assignment.isInTimeInterval(LocalDateTime.parse(timeToCheck, FORMATTER));
+
+        assertFalse(isInInterval);
+    }
+
+    @Test
+    void givenAssignment_whenStartIsInTimeCollision_theTrue() {
+        assignment = new Assignment(null, null, startTime, endTime);
+        String startToCheck = "2020-01-01 10:30:00";
+        String endToCheck = "2020-01-01 11:30:00";
+        LocalDateTime start = LocalDateTime.parse(startToCheck, FORMATTER);
+        LocalDateTime end = LocalDateTime.parse(endToCheck, FORMATTER);
+        Assignment assignmentToCheck = new Assignment(null, null, start, end);
+
+        boolean isInInterval = assignment.isInTimeCollision(assignmentToCheck);
+
+        assertTrue(isInInterval);
+    }
+
+    @Test
+    void givenAssignment_whenEndIsInTimeCollision_thenTrue() {
+        assignment = new Assignment(null, null, startTime, endTime);
+        String startToCheck = "2020-01-01 09:30:00";
+        String endToCheck = "2020-01-01 10:30:00";
+        LocalDateTime start = LocalDateTime.parse(startToCheck, FORMATTER);
+        LocalDateTime end = LocalDateTime.parse(endToCheck, FORMATTER);
+        Assignment assignmentToCheck = new Assignment(null, null, start, end);
+
+        boolean isInInterval = assignment.isInTimeCollision(assignmentToCheck);
+
+        assertTrue(isInInterval);
+    }
+
+    @Test
+    void givenAssignment_whenIsInTimeCollision_thenFalse() {
+        // test
+        assignment = new Assignment(null, null, startTime, endTime);
+        String startToCheck = "2020-01-01 08:00:00";
+        String endToCheck = "2020-01-01 09:00:00";
+        LocalDateTime start = LocalDateTime.parse(startToCheck, FORMATTER);
+        LocalDateTime end = LocalDateTime.parse(endToCheck, FORMATTER);
+        Assignment assignmentToCheck = new Assignment(null, null, start, end);
+
+        boolean isInInterval = assignment.isInTimeCollision(assignmentToCheck);
+
+        assertFalse(isInInterval);
+    }
+}
